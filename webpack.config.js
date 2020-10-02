3e8478a9637789d5e98d32f2dc5a88ee38cea12c
tycoon <smizidor@gmail.com>
2020-10-03 01:39:12 +0200

    autoprefixer added

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,59 @@
-module.exports = (env, argv) => {
-    function isDevelopment() {
-        return argv.mode === 'development';
-    }
-    var config = {
-        entry: './src/index.js',
-        output: {
-            filename: 'bundle.js'
-        },
-        devtool: isDevelopment() ? 'eval-cheap-module-source-map' : 'source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
+const autoprefixer = require('autoprefixer');
+
+module.exports =  (env, argv) => {
+	function isDevelopment() {
+		return argv.mode === 'development';
+	}
+	var config = {
+		entry: './src/index.js',
+		output: {
+			filename: 'bundle.js'
+		},
+		devtool: isDevelopment() ? 'cheap-module-eval-source-map' : 'source-map',
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+								[
+									'@babel/preset-react',
+									{
+										"pragma": "React.createElement",
+										"pragmaFrag": "React.Fragment",
+										"development": isDevelopment()
+									}
+								]
+							]
+						}
+					}
+				},
+				{
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+						'style-loader',
+						'css-loader',
+						{
+                        loader: 'postcss-loader',
                         options: {
-                            presets: [
-                                '@babel/preset-env',
-                                [
-                                    '@babel/preset-react',
-                                    {
-                                        "pragmaFrag": "React.Fragment",
-                                        "pragma": "React.createElement",
-                                        "development": isDevelopment()
-                                    }
+                            postcssOptions: {
+                                plugins: [
+                                  [
+                                    'autoprefixer'
                                 ]
                             ]
                         }
-                    }
-                },
-                {
-                    test: /\.(sa|sc|c)ss$/i,
-                    use:[
-                        'style-loader',
-                        'css-loader',
-                       'sass-loader'
-                    ]
-                }
+                     }
+                    },
+                'sass-loader'    
+                ],
+              }
             ]
         }
-    }
+    };
     return config;
 }
-
-
-
